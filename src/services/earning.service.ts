7c39ba84ab992294feb6152175cdfc46291919ec
sajad paseban <s.pr98@yahoo.com>
2024-03-27 04:41:37 -0700

    new service added

--- src/services/earning.service.ts
+++ src/services/earning.service.ts
@@ -13,6 +13,16 @@ export const getAll = async () =>{
     }   
 }
 
+export const getOne = async (id: any) =>{
+    try{
+        const result = await axios.get(base_api_url('formula/earning/' + id)).then(res => res);
+
+        return result;
+    }catch(res: any){
+        return res.response
+    }   
+}
+
 export const store = async (data: any) => {
     try{
 
@@ -35,6 +45,24 @@ export const destroy = async (data: any) => {
     try{
 
         const result = await axios.delete(base_api_url('formula/earning/'+data)).then(res => res)
+
+        return result
+
+    }catch(res: any){
+        return res.response
+    }
+}
+
+export const edit = async (data: any, id: any) => {
+    try{
+
+        const result = await axios.put(base_api_url('formula/earning/'+id),data, {
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/x-www-form-urlencoded",
+                "X-Requested-With": "XMLHttpRequest",
+            }
+        }).then(res => res)
 
         return result
 
