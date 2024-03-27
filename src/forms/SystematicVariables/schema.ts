a39f1befcc50abe5d04c8bfd9d58509cd1258cda
sajad paseban <s.pr98@yahoo.com>
2024-03-27 04:40:10 -0700

    schema updated

--- src/forms/SystematicVariables/schema.ts
+++ src/forms/SystematicVariables/schema.ts
@@ -3,6 +3,8 @@ import * as yup from 'yup'
 const FormSchema = yup.object({
     name: yup.string().required('فیلد نام متغیر خالی می باشد'),
     key: yup.string().required('فیلد کلید متغیر خالی می باشد'),
+    type: yup.string().required('گزینه ای از فیلد نوع متغیر را مشخص نمایید'),
+    query: yup.string().required('فیلد کوئری خالی می باشد'),
     body: yup.string().required('فیلد دیتا متغیر خالی می باشد'),
     information: yup.string().required('فیلد تعریف متغیر خالی می باشد'),
 })
