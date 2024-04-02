4dfaff01357e6600c66cf48b522eaf449fce1a6e
sajad paseban <s.pr98@yahoo.com>
2024-04-02 01:46:08 -0700

    new fields added

--- src/views/FormulaEditView.vue
+++ src/views/FormulaEditView.vue
@@ -17,6 +17,49 @@
                     <Field name="title" id="title" v-model="form.params.title" class="form-control form-control-sm" />
                     <ErrorMessage name="title" />
                 </div> 
+                <div class="form-group mx-3">
+                    <label for="year">
+                        سال
+                    </label>
+                    <Field name="year" @keypress="handelNumber" id="year" v-model="form.params.year" class="form-control form-control-sm" />
+                    <ErrorMessage name="year" />
+                </div> 
+                <div class="form-group mx-3">
+                    <label for="earningtolltype">
+                        ردیف درآمدی
+                    </label>
+                    <Field as="select" name="earningtolltype" id="earningtolltype" v-model="form.params.earningtolltype" class="form-select form-select-sm">
+                        <option v-for="(item, index) in earningtolltypes" :key="index" :value="item.id">
+                            {{ item.Title }}
+                        </option>
+                    </Field>
+                    <ErrorMessage name="earningtolltype" />
+                </div>
+                <div class="form-group mx-3">
+                    <label for="code_floortype">
+                        طبقه
+                    </label>
+                    <Field as="select" name="code_floortype" id="code_floortype" v-model="form.params.code_floortype" class="form-select form-select-sm">
+                        <option v-for="(item, index) in code_floortypes" :key="index" :value="item.id">
+                            {{ item.Title }}
+                        </option>
+                    </Field>
+                    <ErrorMessage name="code_floortype" />
+                </div>
+                <div class="form-group mx-3">
+                    <label for="description">
+                        توضیحات
+                    </label>
+                    <Field as="textarea" name="description" id="description" v-model="form.params.description" class="form-control form-control-sm" />
+                    <ErrorMessage name="description" />
+                </div>
+                <div class="form-group mx-3">
+                    <label for="priority">
+                        الویت
+                    </label>
+                    <Field @keypress="handelNumber" name="priority" id="priority" v-model="form.params.priority" class="form-control form-control-sm" />
+                    <ErrorMessage name="priority" />
+                </div>
                 <ul class="sub-menu">
                     <li>
     
@@ -63,6 +106,8 @@
     import { messages } from '@/helpers/swal';
     import { ToastMessage } from '@/helpers/enums';
     import { getOne, edit } from '@/services/earning.service';
+    import { getAll as earningtolltype_getAll } from '@/services/earningtolltype.service';
+    import { getAll as floortype_getAll } from '@/services/floortype.service';
     import { ErrorMessage, Field, Form } from 'vee-validate';
     import * as yup from 'yup'
     export default defineComponent({
@@ -79,17 +124,27 @@
         },
         data(){
             const schema = yup.object({
-                title: yup.string().required('فیلد عنوان کد درآمدی اجباری می باشد')
+                title: yup.string().required('فیلد عنوان کد درآمدی اجباری می باشد'),
+                year: yup.string().required('فیلد سال اجباری می باشد'),
+                earningtolltype: yup.string().required('فیلد ردیف درآمدی اجباری می باشد'),
+                code_floortype: yup.string().required('فیلد طبقه اجباری می باشد'),
             })
             return {
                 form:{
                     FormSchema: schema,
                     params:{
-                        title: null
+                        title: null,
+                        year: null,
+                        earningtolltype: null,
+                        code_floortype: null,
+                        description: null,
+                        priority: null
                     }
                 },
                 response: null,
-                code: ''
+                code: '',
+                earningtolltypes: null,
+                code_floortypes: null
             }
         },
         methods:{
@@ -119,10 +174,14 @@
 
                 const data = {
                     title: this.form.params.title,
-                    system_type: 1,
+                    Year: this.form.params.year,
+                    earningtolltype_id: this.form.params.earningtolltype,
+                    code_floortype_id: this.form.params.code_floortype,
+                    Description: this.form.params.description,
+                    Priority: this.form.params.priority,
                     variables: JSON.stringify(useVariableStore().variable),
                     process: JSON.stringify(useConditionStore().process_condition),
-                    Body: code
+                    body: code,
                 }
 
                 const result = await edit(data, params.id).then(res => res);
@@ -147,14 +206,21 @@
             }
         },
         async mounted(){
+            this.earningtolltypes = await earningtolltype_getAll().then(res => res.data)
+            this.code_floortypes = await floortype_getAll().then(res => res.data)
+
             const params = this.$route.params;
             const result = await getOne(params.id).then(res => res)
             if(result.status == 200 || result.status == 201){
-                useVariableStore().setValue(JSON.parse(result.data.row.variables))
-                useConditionStore().setValue(JSON.parse(result.data.row.process))
-                this.form.params.title = result.data.row.title
-                this.code = result.data.row.Body
-                
+                useVariableStore().setValue(JSON.parse(result.data.row.formula_earnings.variables))
+                useConditionStore().setValue(JSON.parse(result.data.row.formula_earnings.process))
+                this.form.params.title = result.data.row.formula_earnings.title
+                this.code = result.data.row.formula_earnings.body
+                this.form.params.year = result.data.row.Year
+                this.form.params.earningtolltype = result.data.row.earningtolltype_id
+                this.form.params.code_floortype = result.data.row.code_floortype_id
+                this.form.params.description = result.data.row.Description
+                this.form.params.priority = result.data.row.Priority
             }
         }
         
@@ -170,7 +236,6 @@
             width: calc(250px + 10px);
             position: absolute;
             top: 0;
-            bottom: 0;
             right: 0;
             border-left: 2px solid rgba($color: #000000, $alpha: 0.1);
             h3{
